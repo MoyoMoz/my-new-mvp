a8594db27d580124bb29319d7842a220b23683d8
MoyoMoz <moyotornai@gmail.com>
2023-08-03 23:35:21 -0700

    Added relevent images

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,3 +1,4 @@
+// /Users/somebody/Developer/CapstoneDirectory/CapstoneProject/frontend/src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import FirstPage from './components/FirstPage';
--- frontend/src/components/MandalaButton.js
+++ frontend/src/components/MandalaButton.js
@@ -1,9 +1,37 @@
 import React from 'react';
 import '../styles/MandalaButton.css';
 
+import LogInButtonImg from '../images/LogInButton.png';
+import BuildButtonImg from '../images/build-button-frame-1.svg';
+import PracticeButtonImg from '../images/practice-button-frame-2.svg';
+import StudyButtonImg from '../images/study-button-frame-1.svg';
+
+//  MandalaButton component
 function MandalaButton({label, image, color, onClick}) {
+  let buttonImg;
+  switch (label) {
+    case 'Log In':
+      buttonImg = LogInButtonImg;
+      break;
+    case 'Build':
+      buttonImg = BuildButtonImg;
+      break;
+    case 'Practice':
+      buttonImg = PracticeButtonImg;
+      break;
+    case 'Study':
+      buttonImg = StudyButtonImg;
+      break;
+    case 'Log Out':  // Use a default button for Log Out
+      buttonImg = '';
+      break;
+    default:
+      buttonImg = '';  // Add a default case for safety
+      break;
+  }
+
   return (
-    <button className="mandala-btn" style={{backgroundImage: `url(${image})`}} onClick={onClick}>
+    <button className="mandala-btn" style={{backgroundImage: `url(${buttonImg})`}} onClick={onClick}>
       <span className="btn-label" style={{color: color}}>{label}</span>
     </button>
   );
