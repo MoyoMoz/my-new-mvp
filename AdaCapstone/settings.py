daa850cc06aa5e7658bd8b85f09b26b7ee9aca00
MoyoMoz <moyotornai@gmail.com>
2023-08-09 00:11:20 -0700

    trying to fix my broken code

--- AdaCapstone/settings.py
+++ AdaCapstone/settings.py
@@ -17,7 +17,7 @@ from dotenv import load_dotenv
 #
 # # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'AdaCapstone', 'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'frontend', 'public', 'static')]
 
 
 # Quick-start development settings - unsuitable for production
