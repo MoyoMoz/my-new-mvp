96c4607d68cac31892d84fce11d8a75509540ea7
MoyoMoz <moyotornai@gmail.com>
2023-08-15 00:49:47 -0700

    moved manage.py file

--- manage.py
+++ manage.py
@@ -6,7 +6,9 @@ import sys
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'AdaCapstone.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE',
+                          'MyDjangoCapstoneRoot.settings')
+
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
